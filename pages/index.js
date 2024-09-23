b4931374c26919ffba4ca9047c8e5b9e079aa47e
Júlio César Coutinho de Souza <jcs.caesar@gmail.com>
2024-09-23 20:19:01 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Emily, eu te amo!<br></br>Você é o amor da minha vida<br></br>Se você me ama, dá uma risadinha :D</h1>
+  return (
+    <h1>
+      Emily, eu te amo!<br></br>Você é o amor da minha vida<br></br>Se você me
+      ama, dá uma risadinha :D
+    </h1>
+  );
 }
 
 export default Home;
